c561710a4705ad0e8af9e51b33fc90c28240c781
GadgetHack <it.tanandr@gmail.com>
2019-03-21 23:21:20 +0100

    bmi_calc

---
+++ bmi.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+float bmi (float weight, float height) 
+{
+  return weight / (height  * height);
+}
+
+int main (void)
+
+{
+    float weight = 0.0;
+    float height = 0.0;
+    float BMI = 0.0;
+
+    printf ("Please enter your weight in kilograms: ");
+    scanf("%f", &weight);
+    printf("Please enter your height in centimeters: ");
+    scanf("%f", &height);
+
+    BMI = bmi(weight, height / 100);
+    if(BMI <= 18.5)
+    {
+        printf("\nYour BMI is %f: Underweight.\n",BMI);
+    }
+    else if(BMI <= 24.99)
+    {
+        printf("\nYour BMI is %f: Normal.\n",BMI);
+    }
+    else if(BMI <= 29.99)
+    {
+        printf("\nYour BMI is %f: Overweight.\n",BMI);
+    }
+    else
+    {
+        printf("\nYour BMI is %f: Obesity.\n",BMI);
+    }
+
+
+    return 0;
+}
